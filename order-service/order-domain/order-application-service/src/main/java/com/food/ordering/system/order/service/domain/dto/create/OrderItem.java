132defe35c8ef18f070cd9d5139a78795e8c062b
marcio.santos <marcio.santos@neodent.com>
2025-01-16 20:17:22 -0300

    creating command classes

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/create/OrderItem.java
@@ -0,0 +1,24 @@
+package com.food.ordering.system.order.service.domain.dto.create;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class OrderItem {
+    @NotNull
+    private final UUID productId;
+    @NotNull
+    private final Integer quantity;
+    @NotNull
+    private BigDecimal price;
+    @NotNull
+    private BigDecimal subTotal;
+}
